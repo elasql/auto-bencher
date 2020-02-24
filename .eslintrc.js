61a6900437b385d54c65d8edb9b80545ba329385
Pin-Yu Wang <pywang@datalab.cs.nthu.edu.tw>
2020-02-24 17:31:37 +0800

    Rename properties files with suffix default

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    "semi":[2, "always"],
+    "quotes":[2, "single", {
+      "avoidEscape": true,
+      "allowTemplateLiterals": true
+    }],
+    "indent": ["error", 2]
+  }
+}

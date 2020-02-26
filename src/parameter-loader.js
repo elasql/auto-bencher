93b3a09dd3953efaa6f47da7e8fca643d30aa482
Pin-Yu Wang <pywang@datalab.cs.nthu.edu.tw>
2020-02-26 16:50:10 +0800

    WIP

--- src/parameter-loader.js
+++ src/parameter-loader.js
@@ -3,7 +3,10 @@ const { loadToml } = require('./utils');
 class ParameterLoader {
   constructor (filePath) {
     this.param = loadToml(filePath);
+    this.params = 
   }
+
+  parseParams()
 }
 
 module.exports = ParameterLoader;

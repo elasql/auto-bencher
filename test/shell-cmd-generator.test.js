9a371f71574920d7ee2aad033634ec5cb14a4dc6
Pin-Yu Wang <pywang@datalab.cs.nthu.edu.tw>
2020-03-02 10:27:07 +0800

    Use string variables to make test consistent

--- test/shell-cmd-generator.test.js
+++ test/shell-cmd-generator.test.js
@@ -3,72 +3,88 @@ const assert = require('chai').assert;
 const ShellCmdGenerator = require('../src/shell-cmd-generator');
 
 describe('ShellCmdGenerator', () => {
-  const cmdGen = new ShellCmdGenerator('db-team', '140.114.87.87');
+  const userName = 'db-team';
+  const ip = '140.114.87.87';
+  const cmdGen = new ShellCmdGenerator(userName, ip);
+
+  const workDir = 'wordDir';
+  const jdkPackageName = 'jdkPackageName';
 
   describe('getScp', () => {
-    it('should return a correct command  without -r', () => {
-      const cmd = cmdGen.getScp(false, 'localPath', 'remotePath');
-      const ans = 'scp localPath db-team@140.114.87.87:remotePath';
+    const localPath = 'localPath';
+    const remotePath = 'remotePath';
+
+    it('should return a correct command without -r', () => {
+      const cmd = cmdGen.getScp(false, localPath, remotePath);
+      const ans = `scp ${localPath} ${userName}@${ip}:${remotePath}`;
       assert.equal(cmd, ans);
     });
 
-    it('should return a correct command  with -r', () => {
-      const cmd = cmdGen.getScp(true, 'localPath', 'remotePath');
-      const ans = 'scp -r localPath db-team@140.114.87.87:remotePath';
+    it('should return a correct command with -r', () => {
+      const cmd = cmdGen.getScp(true, localPath, remotePath);
+      const ans = `scp -r ${localPath} ${userName}@${ip}:${remotePath}`;
       assert.equal(cmd, ans);
     });
   });
 
   describe('getMkdir', () => {
-    it('should return a correct command ', () => {
-      const cmd = ShellCmdGenerator.getMkdir('workDir', 'dir');
-      const ans = 'mkdir -p workDir/dir';
+    const dir = 'dir';
+
+    it('should return a correct command', () => {
+      const cmd = ShellCmdGenerator.getMkdir(workDir, dir);
+      const ans = `mkdir -p ${workDir}/${dir}`;
       assert.equal(cmd, ans);
     });
   });
 
   describe('getJavaVersion', () => {
-    it('should return a correct command ', () => {
-      const cmd = ShellCmdGenerator.getJavaVersion('workDir', 'jdkDir');
-      const ans = 'workDir/jdkDir/bin/java -version';
+    const jdkDir = 'jdkDir';
+
+    it('should return a correct command', () => {
+      const cmd = ShellCmdGenerator.getJavaVersion(workDir, jdkDir);
+      const ans = `${workDir}/${jdkDir}/bin/java -version`;
       assert.equal(cmd, ans);
     });
   });
 
   describe('getTar', () => {
-    it('should return a correct command ', () => {
-      const cmd = ShellCmdGenerator.getTar('workDir', 'jdkPackageName');
-      const ans = 'tar -C workDir -zxf workDir/jdkPackageName';
+
+    it('should return a correct command', () => {
+      const cmd = ShellCmdGenerator.getTar(workDir, jdkPackageName);
+      const ans = `tar -C ${workDir} -zxf ${workDir}/${jdkPackageName}`;
       assert.equal(cmd, ans);
     });
   });
 
   describe('getRm', () => {
-    it('should return a correct command ', () => {
-      const cmd = ShellCmdGenerator.getRm('workDir', 'jdkPackageName');
-      const ans = 'rm workDir/jdkPackageName';
+    it('should return a correct command', () => {
+      const cmd = ShellCmdGenerator.getRm(workDir, jdkPackageName);
+      const ans = `rm ${workDir}/${jdkPackageName}`;
       assert.equal(cmd, ans);
     });
   });
 
   describe('getSsh', () => {
-    it('should return a correct command ', () => {
+    it('should return a correct command', () => {
       const cmd = cmdGen.getSsh('ls -al');
-      const ans = 'ssh db-team@140.114.87.87 ls -al';
+      const ans = `ssh ${userName}@${ip} ls -al`;
       assert.equal(cmd, ans);
     });
   });
 
   describe('getCp', () => {
+    const src = 'src';
+    const dest = 'dest';
+
     it('should return a correct command without -r', () => {
-      const cmd = cmdGen.getCp(false, 'src', 'dest');
-      const ans = 'cp src dest';
+      const cmd = cmdGen.getCp(false, src, dest);
+      const ans = `cp ${src} ${dest}`;
       assert.equal(cmd, ans);
     });
 
     it('should return a correct command with -r', () => {
-      const cmd = cmdGen.getCp(true, 'src', 'dest');
-      const ans = 'cp -r src dest';
+      const cmd = cmdGen.getCp(true, src, dest);
+      const ans = `cp -r ${src} ${dest}`;
       assert.equal(cmd, ans);
     });
   });

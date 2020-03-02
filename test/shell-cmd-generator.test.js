65c46d187713c0f87eb5f67c1821538232282e45
Pin-Yu Wang <pywang@datalab.cs.nthu.edu.tw>
2020-03-02 10:36:40 +0800

    Pass eslint

--- test/shell-cmd-generator.test.js
+++ test/shell-cmd-generator.test.js
@@ -16,14 +16,14 @@ describe('ShellCmdGenerator', () => {
 
     it('should return a correct command without -r', () => {
       const cmd = cmdGen.getScp(false, localPath, remotePath);
-      const ans = `scp ${localPath} ${userName}@${ip}:${remotePath}`;
-      assert.equal(cmd, ans);
+      const expected = `scp ${localPath} ${userName}@${ip}:${remotePath}`;
+      assert.equal(cmd, expected);
     });
 
     it('should return a correct command with -r', () => {
       const cmd = cmdGen.getScp(true, localPath, remotePath);
-      const ans = `scp -r ${localPath} ${userName}@${ip}:${remotePath}`;
-      assert.equal(cmd, ans);
+      const expected = `scp -r ${localPath} ${userName}@${ip}:${remotePath}`;
+      assert.equal(cmd, expected);
     });
   });
 
@@ -32,8 +32,8 @@ describe('ShellCmdGenerator', () => {
 
     it('should return a correct command', () => {
       const cmd = ShellCmdGenerator.getMkdir(workDir, dir);
-      const ans = `mkdir -p ${workDir}/${dir}`;
-      assert.equal(cmd, ans);
+      const expected = `mkdir -p ${workDir}/${dir}`;
+      assert.equal(cmd, expected);
     });
   });
 
@@ -42,33 +42,32 @@ describe('ShellCmdGenerator', () => {
 
     it('should return a correct command', () => {
       const cmd = ShellCmdGenerator.getJavaVersion(workDir, jdkDir);
-      const ans = `${workDir}/${jdkDir}/bin/java -version`;
-      assert.equal(cmd, ans);
+      const expected = `${workDir}/${jdkDir}/bin/java -version`;
+      assert.equal(cmd, expected);
     });
   });
 
   describe('getTar', () => {
-
     it('should return a correct command', () => {
       const cmd = ShellCmdGenerator.getTar(workDir, jdkPackageName);
-      const ans = `tar -C ${workDir} -zxf ${workDir}/${jdkPackageName}`;
-      assert.equal(cmd, ans);
+      const expected = `tar -C ${workDir} -zxf ${workDir}/${jdkPackageName}`;
+      assert.equal(cmd, expected);
     });
   });
 
   describe('getRm', () => {
     it('should return a correct command', () => {
       const cmd = ShellCmdGenerator.getRm(workDir, jdkPackageName);
-      const ans = `rm ${workDir}/${jdkPackageName}`;
-      assert.equal(cmd, ans);
+      const expected = `rm ${workDir}/${jdkPackageName}`;
+      assert.equal(cmd, expected);
     });
   });
 
   describe('getSsh', () => {
     it('should return a correct command', () => {
       const cmd = cmdGen.getSsh('ls -al');
-      const ans = `ssh ${userName}@${ip} ls -al`;
-      assert.equal(cmd, ans);
+      const expected = `ssh ${userName}@${ip} ls -al`;
+      assert.equal(cmd, expected);
     });
   });
 
@@ -78,14 +77,14 @@ describe('ShellCmdGenerator', () => {
 
     it('should return a correct command without -r', () => {
       const cmd = cmdGen.getCp(false, src, dest);
-      const ans = `cp ${src} ${dest}`;
-      assert.equal(cmd, ans);
+      const expected = `cp ${src} ${dest}`;
+      assert.equal(cmd, expected);
     });
 
     it('should return a correct command with -r', () => {
       const cmd = cmdGen.getCp(true, src, dest);
-      const ans = `cp -r ${src} ${dest}`;
-      assert.equal(cmd, ans);
+      const expected = `cp -r ${src} ${dest}`;
+      assert.equal(cmd, expected);
     });
   });
 });

a4273c569f3f161c4e81fdb8631bf7146ea22c07
Pin-Yu Wang <pywang@datalab.cs.nthu.edu.tw>
2020-03-03 10:42:59 +0800

    Add comment

--- src/child-process.js
+++ src/child-process.js
@@ -5,12 +5,23 @@ const util = require('util');
 const childProcessExec = util.promisify(require('child_process').exec);
 
 async function exec (cmd) {
-  /*
-  please do NOT try catch childProcessExec
+/*
+  PLEASE DO NOT TRY CATCH childProcessExec !!!!!
+
   a lot of functions depend on the return value and error of childProcessExec
   if you catch the error in this function
   it will cause disaster
-  */
+
+  result will be {stdout:'...', stderr:''} if childProcessExec is resolved\
+  else it will be {
+    killed: false,
+    code: 1,
+    signal: null,
+    cmd: 'your cmd',
+    stdout: '',
+    stderr: ''
+  }
+*/
   const result = await childProcessExec(cmd);
   return result;
 };

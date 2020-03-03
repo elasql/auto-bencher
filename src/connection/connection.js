722a09a32738e6c5015e93d829393e67565b1f5c
Pin-Yu Wang <pywang@datalab.cs.nthu.edu.tw>
2020-03-03 11:14:43 +0800

    Remove legacy code

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -49,47 +49,6 @@ class Connection {
   }
 }
 
-module.exports = Connection;
-
-/*
 module.exports = {
-    Action: Action,
-    ConnectionInfo: ConnectionInfo,
-    generate_connection_list: generate_connection_list
+  Connection: Connection
 };
-function ConnectionInfo(id, ip, port, prop){
-    this.id = id;
-    this.ip = ip;
-    this.port = port;
-    this.prop = prop;
-    this.to_string = function(){
-        return this.id + '\\ ' + this.ip + '\\ ' + this.port;
-    }
-}
-function generate_connection_list(ip_list, conn_count, max_conn_per_ip, prop){
-    list = [];
-    id = 0;
-    conn_per_node = 1;
-    flag = false;
-    while(true){
-        for(i in ip_list){
-            list.push(new ConnectionInfo(id, ip_list[i], INIT_PORT + conn_per_node - 1, prop));
-
-            id += 1;
-            if (id >= conn_count) {
-                flag = true;
-                break;
-            }
-        }
-        if(flag)
-            break;
-
-        conn_per_node += 1;
-        if(conn_per_node > max_conn_per_ip) {
-            throw new Error("The number of machines is not enough.");
-        }
-    }
-    return list;
-}
-
-*/

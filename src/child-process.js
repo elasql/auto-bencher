904d93fb0ba0402c178058a21311779045890164
Pin-Yu Wang <pywang@datalab.cs.nthu.edu.tw>
2020-03-03 10:35:34 +0800

    Update child-process

--- src/child-process.js
+++ src/child-process.js
@@ -5,7 +5,14 @@ const util = require('util');
 const childProcessExec = util.promisify(require('child_process').exec);
 
 async function exec (cmd) {
-  await childProcessExec(cmd);
+  /*
+  please do NOT try catch childProcessExec
+  a lot of functions depend on the return value and error of childProcessExec
+  if you catch the error in this function
+  it will cause disaster
+  */
+  const result = await childProcessExec(cmd);
+  return result;
 };
 
 module.exports = {

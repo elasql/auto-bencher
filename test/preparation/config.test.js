0c693ab30a3f31a79fa9f57e75492b2effd752b9
Pin-Yu Wang <pywang@datalab.cs.nthu.edu.tw>
2020-03-17 17:28:56 +0800

    Complete config.js

--- test/preparation/config.test.js
+++ test/preparation/config.test.js
@@ -1,11 +1,12 @@
 const assert = require('chai').assert;
 
-const { loadToml } = require('../src/utils');
-const Config = require('../src/config');
+const { loadToml } = require('../../src/utils');
+const Config = require('../../src/preparation/config');
 
 describe('Config', () => {
   const tomlObject = loadToml('./test/test-toml/config.test.toml');
   const config = new Config(tomlObject);
+
   const {
     jdkDir,
     jdkPackageName,

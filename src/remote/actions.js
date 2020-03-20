6e847f1f6dcbe1e843e62e2c33dd3822f6199209
Pin-Yu Wang <pywang@datalab.cs.nctu.edu.tw>
2020-03-20 15:07:55 +0800

    WIP

--- src/remote/actions.js
+++ src/remote/actions.js
@@ -1,13 +1,80 @@
 const Cmd = require('../cmd/cmd-generator');
 const { exec } = require('../cmd/cmd-executor');
+const logger = require('../logger');
+
+const Action = {
+  loading: 1,
+  benchmarking: 2
+};
+
+const delay = (interval) => {
+  return new Promise(resolve => setTimeout(resolve, interval));
+};
 
 // TODO: add logger.debug
 
-async sendBenchDirToRemote (remoteWorkDir) {
-  const scp = Cmd.scp(true, 'benchmarker', remoteWorkDir);
+async function sendDir (localPath, remoteWorkDir, remoteInfo) {
+  const scp = Cmd.scp(true, localPath, remoteWorkDir);
+  // don't try catch here
+  // let it error
+  await exec(scp);
 }
 
-async function checkLogOnRemote (cmd, keyword, logPath) {
+async function deleteDir (cmd, dir, remoteInfo) {
+  const { prefix, id, ip } = remoteInfo;
+  const rm = Cmd.rm(true, dir);
+  const ssh = cmd.ssh(rm);
+
+  try {
+    await exec(ssh);
+  } catch (err) {
+    if (err.code === 1) {
+      logger.info(`${dir} is not found on ${prefix} ${id} ${ip}`);
+    } else {
+      throw Error(err.stderr);
+    }
+  }
+}
+async function cleanDir (cmd, dir, remoteInfo) {
+  const { prefix, id, ip } = remoteInfo;
+  const rm = Cmd.rm(true, dir);
+  const ssh = cmd.ssh(rm);
+
+  try {
+    await exec(ssh);
+  } catch (err) {
+    if (err.code === 1) {
+      logger.info(`${dir} is not found on ${prefix} ${id} ${ip}`);
+    } else {
+      throw Error(err.stderr);
+    }
+  }
+}
+
+async function runJar (cmd, action, javaBin, vmArgs, jarPath, logPath, remoteInfo) {
+  const { prefix, id, ip } = remoteInfo;
+  const progArgs = `${id} ${action}`;
+  const runJar = Cmd.runJar(
+    javaBin,
+    vmArgs,
+    jarPath,
+    progArgs,
+    logPath
+  );
+
+  const ssh = cmd.ssh(runJar);
+  logger.info(`${prefix} ${id} ${ip} is running`);
+
+  try {
+    await exec(ssh);
+  } catch (err) {
+    throw Error(err.message);
+  }
+}
+
+
+
+async function checkLog (cmd, keyword, logPath) {
   const grep = Cmd.grep(keyword, logPath);
   const ssh = cmd.ssh(grep);
   // don't try catch here, let outside functions to handle
@@ -16,25 +83,31 @@ async function checkLogOnRemote (cmd, keyword, logPath) {
   return result;
 }
 
-async function checkErrorOnRemote (cmd, keyword, logPath, id, isServer) {
+async function checkError (cmd, keyword, logPath, remoteInfo) {
+  const { prefix, id, ip } = remoteInfo;
   let result;
   try {
-    result = await checkRemoteLog(cmd, keyword, logPath);
+    result = await checkLog(cmd, keyword, logPath);
   } catch (err) {
     // grep will return 1 if it greps nothing
     if (err.code === 1) {
       return;
     }
-    throw Error(err.message);
+    throw Error(err.stderr);
   }
 
   // grep error keyword on the remote, so throw an error
   const { stdout } = result;
-  const prefix = isServer ? 'server' : 'client';
-  throw Error(`grep error on ${prefix} ${id} - ${stdout}`);
+  throw Error(`grep error on ${prefix} ${id} ${ip} - ${stdout}`);
 }
 
 module.exports = {
-  checkRemoteError,
-  checkRemoteLog
-}
+  Action,
+  delay,
+  sendDir,
+  deleteDir,
+  cleanDir,
+  runJar,
+  checkError,
+  checkLog
+};
